626f62b9a19aab325b93209c19751f7b579d3006
AnikaitLakhotia <117095969+AnikaitLakhotia@users.noreply.github.com>
2023-09-20 12:22:24 -0600

    Added additional comments and cleaned the code.

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from encoding import encoding
 from verifier import verifier
 if __name__ == '__main__':
-    encoding, cumulative_dict = encoding(18, 3)
-    print(encoding)
-    cumulative_string = ""
-    print(verifier(cumulative_string, cumulative_dict, 18))
+
+    # Specify parameters of the encoding
+    encoding, cumulative_dict = encoding(6, 2, 2, 4)
+
+    # Insert the path to the file where the cnf formula is to be stored
+    file_path = "your_file_path"
+
+    # Open file and write data to it
+    with open(file_path, "w") as file:
+        file.write(encoding)
+
+    # If solver outputs SAT, insert the SAT assignment
+    assignment_string = "insert_assignment_if_sat"
+
+    # Print verification result
+    print(verifier(assignment_string, cumulative_dict, 6))
