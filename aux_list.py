d50c9a6ac03e5659b344607352bbc117034cf53c
AnikaitLakhotia <alokhoti@uwaterloo.ca>
2023-11-30 21:14:28 +0530

    Move exception handling under try statement in aux_list

--- aux_list.py
+++ aux_list.py
@@ -16,22 +16,23 @@ def create_aux_list(t, num_t, shift):
            but also with respect to values for other variables in the encoding.
     """
 
-    # Input validation and value checks
-    for arg_name, arg_value, min_value in zip(('t', 'num_t', 'shift'), (t, num_t, shift), (1, 2, 0)):
-        if not isinstance(arg_value, int):
-            raise TypeError(f'The {arg_name} argument must be an integer.')
-
-        if arg_value < min_value:
-            raise ValueError(f'Invalid value for {arg_name}. It must be greater than {min_value}.')
-
-    # Initialize count based on the shift
-    count = shift + 1
-    list_aux = []
-
     try:
+        # Input validation and value checks
+        for arg_name, arg_value, min_value in zip(('t', 'num_t', 'shift'), (t, num_t, shift), (1, 2, 0)):
+            if not isinstance(arg_value, int):
+                raise TypeError(f'The {arg_name} argument must be an integer.')
+
+            if arg_value < min_value:
+                raise ValueError(f'Invalid value for {arg_name}. It must be greater than {min_value}.')
+
+        # Initialize count based on the shift
+        count = shift + 1
+        list_aux = []
+
         # Loop to generate auxiliary variables
         for i in range(0, t * (num_t - 1)):
             list_aux.append(count + i)
+
     except Exception as e:
         # Handle any unexpected exceptions
         raise RuntimeError(f"An error occurred while running create_aux_list: {e}")
