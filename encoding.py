91945e911d7a1005891224f74d02182fe0c650e7
AnikaitLakhotia <alokhoti@uwaterloo.ca>
2023-12-08 17:30:00 +0530

    Adapt streamlining 0 for commutativity

--- encoding.py
+++ encoding.py
@@ -126,7 +126,7 @@ def encoding(num_t, num_row_1, num_col_1, num_col_2, commutative, lex_order,
             else:
                 # Create the cumulative list of SAT encoding clauses using alternate scheme.
                 cumulative_list, num_aux_var = create_encoding_list_v2(cumulative_dict, num_t, num_row_1,
-                                                                       num_col_1, num_col_2)
+                                                                       num_col_1, num_col_2, commutative)
                 num_var += num_aux_var
 
         else:
@@ -156,11 +156,19 @@ def encoding(num_t, num_row_1, num_col_1, num_col_2, commutative, lex_order,
                     cumulative_dict[key] = value
 
             num_var = len(cumulative_dict)
-            # Create the cumulative list of commutative encoding clauses.
-            cumulative_list = create_commutative_encoding_list(cumulative_dict, num_t, num_row_1, num_col_1, num_col_2)
-            num_var += (num_t - 1) * (((num_row_1**2) * (num_col_2**2) * (num_col_1**2)) +
-                                      ((num_row_1**3) * (num_col_1**2) * num_col_2) +
-                                      (num_row_1 * (num_col_1**2) * (num_col_2**3)))
+
+            if not streamlining_0:
+                # Create the cumulative list of commutative encoding clauses.
+                cumulative_list = create_commutative_encoding_list(cumulative_dict, num_t, num_row_1, num_col_1, num_col_2)
+                num_var += (num_t - 1) * (((num_row_1**2) * (num_col_2**2) * (num_col_1**2)) +
+                                          ((num_row_1**3) * (num_col_1**2) * num_col_2) +
+                                          (num_row_1 * (num_col_1**2) * (num_col_2**3)))
+
+            else:
+                # Create the cumulative list of SAT encoding clauses using alternate scheme.
+                cumulative_list, num_aux_var = create_encoding_list_v2(cumulative_dict, num_t, num_row_1,
+                                                                       num_col_1, num_col_2, commutative)
+                num_var += num_aux_var
 
         lex_string = ""
         num_lex_clauses = 0
