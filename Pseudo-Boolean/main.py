2d239bd93609ddb33db25d24dc24f988e9eec0be
aayushkapoor1 <60718036+aayushkapoor1@users.noreply.github.com>
2024-06-14 01:53:31 -0400

    Update main.py

--- Pseudo-Boolean/main.py
+++ Pseudo-Boolean/main.py
@@ -1,9 +1,14 @@
-from encoding import PB
+from encoding import NaiveEncoding, OptimizedEncoding
 import sys
 
-_, number_of_multiplications, m, n, p, s, c = sys.argv
+_, number_of_multiplications, m, n, p, s, c, translator_type, encoding_type, id = sys.argv
+pb = 0
+if encoding_type == "0":
+    pb = NaiveEncoding(int(number_of_multiplications), int(
+    m), int(n), int(p), int(s), int(c), int(translator_type), int(encoding_type), id)
+elif encoding_type == "1":
+    pb = OptimizedEncoding(int(number_of_multiplications), int(
+    m), int(n), int(p), int(s), int(c), int(translator_type), int(encoding_type), id)
 
-pb = PB(int(number_of_multiplications), int(
-    m), int(n), int(p), int(s), int(c))
 pb.create_encoding()
 pb.opb_file.close()
